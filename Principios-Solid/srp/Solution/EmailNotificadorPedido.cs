11f25016f638b606cf681d94cdf12ec24baac9ae
Camila Nunes <cvn.camila@outlook.com>
2025-04-17 12:06:08 -0300

    removendo classes não usadas;

--- Principios-Solid/srp/Solution/EmailNotificadorPedido.cs
+++ Principios-Solid/srp/Solution/EmailNotificadorPedido.cs
@@ -1,10 +1,5 @@
 ﻿using srp.Problem;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace srp.Solution
 {
--- Principios-Solid/srp/Solution/SqlPedidoRepository.cs
+++ Principios-Solid/srp/Solution/SqlPedidoRepository.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
 using srp.Problem;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace srp.Solution
 {
